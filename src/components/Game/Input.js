dfc6056e1e758786731d1e658a84425dbc261e01
ruyi <tan_ruyi@hotmail.com>
2023-11-30 09:23:52 +0800

    - exercise 1 done

---
+++ src/components/Game/Input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Input() {
+	const [input, setInput] = React.useState('');
+
+	function inputController(event) {
+		setInput(event.target.value.toUpperCase());
+	}
+
+	function handleFormSubmit(event) {
+		event.preventDefault();
+
+		console.info(input);
+
+		setInput('');
+	}
+
+	return (
+		<form className='guess-input-wrapper' onSubmit={handleFormSubmit}>
+			<label html-for='guess-input'>Enter guess:</label>
+			<input id='guess-input' type='text' pattern='[A-Za-z]{5}' title='Must contain only 5 letters' value={input} onChange={inputController} />
+		</form>
+	);
+}
+
+export default Input;
